eede9a87b20d4e3397790791b6bfd90a8eed4a03
otlet <otlet@otlet.pl>
2018-10-05 22:52:54 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/jezu.jpg">
+    <HelloWorld msg="Czy Mam Internet?"/>
   </div>
 </template>
 
---
+++ src/components/AdBlock.vue
@@ -0,0 +1,19 @@
+<script>
+    import adblock from 'vue-adblock'
+ 
+export default {
+  name: 'app',
+  components: {
+    adblock
+  },
+  data () {
+    return {
+      add:''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
